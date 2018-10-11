ca9b6f4bfbfb1bd1b4db602f12b42f6bc4e7ee85
caniiggia <caniiggia2010@hotmail.com>
2018-10-11 03:13:36 -0400

    Aperfeiçoamento do codigo

--- rec/Sistema.py
+++ rec/Sistema.py
@@ -22,7 +22,6 @@ def recommend(username, users):
     #aqui encontraremos os vizinhos proximos
     proximos = computeNearestNeighbor(username, users)[0][1]
     recomendacoes = []
-    #agora encontramos as bandas dos vizinhos que o usuario não ouvio
     neighborRatings = users[proximos]
     userRatings = users[username]
     for artista in neighborRatings:
@@ -32,23 +31,33 @@ def recommend(username, users):
                   key=lambda artistTuple: artistTuple[1],
                   reverse = True)
 
-users = usuario()
-filtros = categoria()
+def Principal():
+    #Pega os dados no BD:
+    users = usuario()
+    #parte dos inputs do usuario:
+    Pessoa= input("digite seu nome:")
+    Lugar= input("digite seu lugar turistico 1:")
+    Nota1= int(input("diigte a nota:"))
+    Lugar2= input("digite seu lugar turistico 2:")
+    Nota2 = int(input("digite a nota 2:"))
+    #criação do dicionario do usuario:
+    Lugares=[Lugar,Lugar2]
+    Notas=[Nota1,Nota2]
+    Novo_Usuario=dict(zip(Lugares,Notas))
+    #adicionando informaçoes do usuario no BD:
+    users[Pessoa]=Novo_Usuario
+    #chamamos a funçao recomendaçao e fazemos a recomendação ao usuario:
+    Recomendacao = recommend(Pessoa,users)
+    return Recomendacao
 
-Pessoa= input("digite seu nome:")
-Lugar= input("digite seu lugar turistico 1:")
-Nota1= int(input("diigte a nota:"))
-Lugar2= input("digite seu lugar turistico 2:")
-Nota2 = int(input("digite a nota 2:"))
-Lugares=[Lugar,Lugar2]
-Notas=[Nota1,Nota2]
-Novo=dict(zip(Lugares,Notas))
-users[Pessoa]=Novo
+#vai pegar os dados das categorias no BD:    
+filtros = categoria()#usa com sabedoria felipe :)
+def Mostrar():
+    #recupera e mostra a recomendaçao ao usuario
+    Rec=Principal()
+    print(Rec)
 
-Recomendacao = recommend(Pessoa,users)
-vizinho= computeNearestNeighbor(Pessoa,users)
-print(vizinho)
-print(Recomendacao)
+Mostrar()
 
      
 
