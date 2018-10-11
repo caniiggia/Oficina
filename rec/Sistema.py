88443ab6e6cafdf16e3c63979cc2bb70d743b646
Felipe Henrique <solelipecosta1@gmail.com>
2018-10-11 13:05:01 -0300

    Chore: Code update + Fake Interface

--- rec/Sistema.py
+++ rec/Sistema.py
@@ -19,7 +19,8 @@ def computeNearestNeighbor(username, users):
     distances.sort()
     return distances
 
-def recommendNoFilter(username, users):
+
+def recommend(username, users, fylter):
     #aqui encontraremos os vizinhos proximos
     proximos = computeNearestNeighbor(username, users)[0][1]
     recomendacoes = []
@@ -27,53 +28,61 @@ def recommendNoFilter(username, users):
     userRatings = users[username]
     for turistico in neighborRatings:
         if not turistico in userRatings:
-            recomendacoes.append((turistico, neighborRatings[turistico]))
-    return sorted(recomendacoes,
-                  key=lambda turisticoTuple: turisticoTuple[1],
-                  reverse = True)
-
-
-def recommendWithFilter(username, users, fylter):
-    #aqui encontraremos os vizinhos proximos
-    proximos = computeNearestNeighbor(username, users)[0][1]
-    recomendacoes = []
-    neighborRatings = users[proximos]
-    userRatings = users[username]
-    for turistico in neighborRatings:
-        if not turistico in userRatings:
-            if turistico in fylter:
+            if fylter != "NAO" and turistico in fylter:
+                recomendacoes.append((turistico, neighborRatings[turistico]))
+            elif fylter == "NAO":
                 recomendacoes.append((turistico, neighborRatings[turistico]))
     return sorted(recomendacoes,
                   key=lambda turisticoTuple: turisticoTuple[1],
                   reverse = True)
 
 def Principal():
+    cont = 0
     #Pega os dados no BD:
     users = usuario()
     #vai pegar os dados das categorias no BD:    
     filtros = categoria()#usa com sabedoria felipe :)
     #parte dos inputs do usuario:
-    Pessoa= input("digite seu nome:")
-    Lugar= input("digite seu lugar turístico 1:")
-    Nota1= int(input("digite a nota:"))
-    Lugar2= input("digite seu lugar turístico 2:")
-    Nota2 = int(input("digite a nota 2:"))
-    opcao = input("Deseja aplicar algum filtro?")
-    opcao = opcao.upper()
-    if(opcao == "SIM"):
-        name = input("Qual categoria procura?")
-        fylter = filtros[name]
+    lugares = []
+    notas = []
+    pessoa= input("digite seu nome:\n")
+    print("\nPerfil: ", pessoa)
+    print("------------------------------------------")
+    while(True):
+        lugar = input("digite seu lugar turístico " + str(cont+1) + ":\n")
+        nota = int(input("digite a nota para " + lugar + ":\n"))
+        escolha = input("Deseja avaliar mais lugares?\n")
+        escolha = escolha.upper()
+        lugares.append(lugar)
+        notas.append(nota)
+        if(escolha=="NÃO" or escolha=="NAO"):
+            print("------------------------------------------")
+            break
+        print("------------------------------------------")
+        cont+=1
+    #Lugar2= input("digite seu lugar turístico 2:")
+    #Nota2 = int(input("digite a nota 2:"))
+    while(True):
+        opcao = input("Deseja aplicar algum filtro?\n")
+        opcao = opcao.upper()
+        if(opcao == "SIM"):
+            name = input("Qual categoria procura?\n")
+            fylter = filtros[name]
+            break
+        elif(escolha=="NÃO" or escolha=="NAO"):
+            fylter = 'NAO'
+            break
+        else:
+            print("Por favor, informe sim ou não como resposta\n")
+        
     #criação do dicionario do usuario:
-    Lugares=[Lugar,Lugar2]
-    Notas=[Nota1,Nota2]
-    Novo_Usuario=dict(zip(Lugares,Notas))
+    #Lugares=[Lugar,Lugar2]
+    #Notas=[Nota1,Nota2]
+    Novo_Usuario=dict(zip(lugares,notas))
     #adicionando informaçoes do usuario no BD:
-    users[Pessoa]=Novo_Usuario
+    users[pessoa]=Novo_Usuario
     #chamamos a funçao recomendaçao e fazemos a recomendação ao usuario:
-    if(opcao == "SIM"):
-        Recomendacao = recommendWithFilter(Pessoa,users,fylter)
-    else:
-        Recomendacao = recommendNoFilter(Pessoa,users)
+    Recomendacao = recommend(pessoa,users,fylter)
     return Recomendacao
 
 def Mostrar():
