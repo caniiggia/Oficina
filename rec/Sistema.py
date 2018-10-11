66b3de619f574162a1938aadc57d641f28ee97b4
caniiggia <caniiggia2010@hotmail.com>
2018-10-11 16:09:54 -0400

    ultima

--- rec/Sistema.py
+++ rec/Sistema.py
@@ -48,12 +48,13 @@ def Principal():
     #parte dos inputs do usuario:
     lugares = []
     notas = []
-    pessoa= input("digite seu nome:\n")
+    pessoa= input("Digite seu nome:\n")
     print("\nPerfil: ", pessoa)
     print("------------------------------------------")
+    Menu_Lugares()
     while(True):
-        lugar = input("digite seu lugar turístico " + str(cont+1) + ":\n")
-        nota = int(input("digite a nota para " + lugar + ":\n"))
+        lugar = input("Digite seu lugar turístico " + str(cont+1) + ":\n")
+        nota = int(input("Digite uma nota (de 0 a 5) para " + lugar + ":\n"))
         escolha = input("Deseja avaliar mais lugares?\n")
         escolha = escolha.upper()
         lugares.append(lugar)
@@ -63,12 +64,11 @@ def Principal():
             break
         print("------------------------------------------")
         cont+=1
-    #Lugar2= input("digite seu lugar turístico 2:")
-    #Nota2 = int(input("digite a nota 2:"))
     while(True):
-        opcao = input("Deseja aplicar algum filtro?\n")
+        opcao = input("Deseja escolher alguma categoria?\n")
         opcao = opcao.upper()
         if(opcao == "SIM"):
+            Menu_Categorias()
             name = input("Qual categoria procura?\n")
             fylter = filtros[name]
             break
@@ -78,15 +78,44 @@ def Principal():
         else:
             print("Por favor, informe sim ou não como resposta\n")
         
-    #criação do dicionario do usuario:
-    #Lugares=[Lugar,Lugar2]
-    #Notas=[Nota1,Nota2]
+    
     Novo_Usuario=dict(zip(lugares,notas))
     #adicionando informaçoes do usuario no BD:
     users[pessoa]=Novo_Usuario
     #chamamos a funçao recomendaçao e fazemos a recomendação ao usuario:
+    print(computeNearestNeighbor(pessoa, users))
     Recomendacao = recommend(pessoa,users,fylter)
     return Recomendacao
+
+def Menu_Categorias():
+    print("Categorias Existentes:\
+          \n1-Bares\
+          \n2-Cultural\
+          \n3-Zoológicos\
+          \n4-Compras\
+          \n5-Esportivo\
+          \n6-Lazer\
+          \n7-Alimentação\n")
+
+def Menu_Lugares():
+    print("Lugares Existentes:\
+          \n1-Teatro Amazonas\
+          \n2-Centro Cultural dos Povos da Amazônia\
+          \n3-Porão do Alemão\
+          \n4-Bar Axerito\
+          \n5-Bosque da Ciência\
+          \n6-CIGS\
+          \n7-Amazonas Shopping\
+          \n8-Mercado Municipal Adolpho Lisboa\
+          \n9-Arena da Amazonia\
+          \n10-Arena Paintball\
+          \n11-Shot in the Dark\
+          \n12-Praia da Ponta Negra\
+          \n13-Cachoeira Alto do Tarumã\
+          \n14-Praça da Saudade\
+          \n15-Parque Cidade da Criança\
+          \n16-Praça de Alimentação do Parque 10\
+          \n17-Praça de Alimentação do Parque das Laranjeiras\n")
 
 def Mostrar():
     #recupera e mostra a recomendaçao ao usuario
